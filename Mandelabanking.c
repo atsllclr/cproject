577fdfc2f9c64324b54d9eec665ee0106a973838
Astonishing technology Solutions, LLC <74583140+atsllclr@users.noreply.github.com>
2021-03-18 05:36:24 -0700

    Add files via upload

---
+++ Mandelabanking.c
@@ -0,0 +1,160 @@
+/******************************************************************************
+
+Mandela Banking System allows user to;
+display all records,
+search a record,
+deposit amount,
+withdraw amount,
+and  exit after transaction.
+
+*******************************************************************************/
+
+#include <stdio.h>
+//Character (data types) to be use with their assigned limit values
+struct customer
+{
+    int account_no;
+    char name[80];
+    int balance;
+};
+// list of options using from 0-1
+void accept(struct customer[], int);
+void display(struct customer[], int);
+int search(struct customer[], int, int);
+void deposit(struct customer[], int, int, int);
+void withdraw(struct customer[], int, int, int);
+
+// Options for user to select from using number displaying from 0-4
+int main()
+{
+    struct customer data[20];
+    int n, choice, account_no, amount, index;
+
+    printf("Banking System\n\n");
+    printf("Number of customer records you want to enter? : ");
+    scanf("%d", &n);
+    accept(data, n);
+    do
+    {
+
+        printf("\nBanking System Menu :\n");
+        printf("Press 1 to display all records.\n");
+        printf("Press 2 to search a record.\n");
+        printf("Press 3 to deposit amount.\n");
+        printf("Press 4 to withdraw amount.\n");
+        printf("Press 0 to exit\n");
+        printf("\nEnter choice(0-4) : ");
+        scanf("%d", &choice);
+        // choices base on user input
+        switch (choice)
+        {
+            case 1:
+                display(data, n);
+                break;
+            case 2:
+                printf("Enter account number to search : ");// allowing user to search for account record
+                scanf("%d", &account_no);
+                index = search(data, n, account_no);
+                if (index ==  - 1)
+                {
+                    printf("Record not found : ");// applying errors when user input is invalid
+                }
+                else
+                {
+                    printf("A/c Number: %d\nName: %s\nBalance: %d\n",
+                        data[index].account_no, data[index].name,
+                        data[index].balance);
+                }
+                break;
+            case 3:
+                printf("Enter account number : ");// entering of account number to search in the system for user access
+                scanf("%d", &account_no);
+                printf("Enter amount to deposit : ");//requesting on amount to be desposite base on user choice
+                scanf("%d", &amount);
+                deposit(data, n, account_no, amount);
+                break;
+            case 4:
+                printf("Enter account number : ");
+                scanf("%d", &account_no);
+                printf("Enter amount to withdraw : ");
+                scanf("%d", &amount);
+                withdraw(data, n, account_no, amount);
+        }
+    }
+    while (choice != 0);
+
+    return 0;
+}
+// storing record in for next insist transaction by user
+void accept(struct customer list[80], int s)
+{
+    int i;
+    for (i = 0; i < s; i++)
+    {
+        printf("\nEnter data for Record #%d", i + 1);
+
+        printf("\nEnter account_no : ");
+        scanf("%d", &list[i].account_no);
+        fflush(stdin);
+        printf("Enter name : ");
+        gets(list[i].name);
+        list[i].balance = 0;
+    }
+}
+
+void display(struct customer list[80], int s)
+{
+    int i;
+
+    printf("\n\nA/c No\tName\tBalance\n");
+    for (i = 0; i < s; i++)
+    {
+        printf("%d\t%s\t%d\n", list[i].account_no, list[i].name,
+            list[i].balance);
+    }
+}
+
+int search(struct customer list[80], int s, int number)
+{
+    int i;
+
+    for (i = 0; i < s; i++)
+    {
+        if (list[i].account_no == number)
+        {
+            return i;
+        }
+    }
+    return  - 1;
+}
+// user to be able to deposit in their account using their account number
+void deposit(struct customer list[], int s, int number, int amt)
+{
+    int i = search(list, s, number);
+    if (i ==  - 1)
+    {
+        printf("Record not found");
+    }
+    else
+    {
+        list[i].balance += amt;
+    }
+}
+//allowing user to withdraw from their amount base on the account number used
+void withdraw(struct customer list[], int s, int number, int amt)
+{
+    int i = search(list, s, number);
+    if (i ==  - 1)
+    {
+        printf("Record not found\n");
+    }
+    // condition that cancel user transaction due to limited amount in account
+    else if (list[i].balance < amt)
+    {
+        printf("Insufficient balance\n");
+    }
+    else
+    {
+        list[i].balance -= amt;
+    }
+}
